57d9f81abc0d57c5fc98dd1f0aa9e8a3879d82fb
Adam Johnson <johnson.adamdouglas@gmail.com>
2022-10-19 16:41:52 -0400

    added user input loop

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,24 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess the number I'm thinking of, 1 through 5.");
+
+            Console.WriteLine("Guess the number I'm thinking of, 1 through 100.");
             var r = new Random();
-            var favNumber = r.Next(1, 5);
-            var userInput = int.Parse(Console.ReadLine());
+            var favNumber = r.Next(1, 100);
+            bool incorrect = true;
+            while (incorrect)
+            {
+                var userInput = int.Parse(Console.ReadLine());
 
-            if (userInput < favNumber)
-            {
-                Console.WriteLine("too low");
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("too low");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("too high");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
             }
-            else if (userInput > favNumber)
-            {
-                Console.WriteLine("too high");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
-
         }
     }
 }
