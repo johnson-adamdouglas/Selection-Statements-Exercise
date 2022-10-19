e7894fa13e5170dc8d44546d6f095e3680df9675
Adam Johnson <johnson.adamdouglas@gmail.com>
2022-10-19 16:45:44 -0400

    added guess again prompt

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Guess the number I'm thinking of, 1 through 100.");
+            Console.WriteLine("Guess the number I'm thinking of, 1 through 1000.");
             var r = new Random();
-            var favNumber = r.Next(1, 100);
+            var favNumber = r.Next(1, 1000);
             bool incorrect = true;
             while (incorrect)
             {
@@ -15,11 +15,11 @@
 
                 if (userInput < favNumber)
                 {
-                    Console.WriteLine("too low");
+                    Console.WriteLine("too low, guess again");
                 }
                 else if (userInput > favNumber)
                 {
-                    Console.WriteLine("too high");
+                    Console.WriteLine("too high, guess again");
                 }
                 else
                 {
