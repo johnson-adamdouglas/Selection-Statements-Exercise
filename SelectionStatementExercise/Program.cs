cc210a646a51bec212bd9a7aa5229b563ef939b7
Adam Johnson <johnson.adamdouglas@gmail.com>
2022-10-19 15:29:53 -0400

    completed exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Guess the number I'm thinking of, 1 through 5.");
+            var r = new Random();
+            var favNumber = r.Next(1, 5);
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
         }
     }
 }
